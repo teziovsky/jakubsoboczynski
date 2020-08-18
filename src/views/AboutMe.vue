1052dd526e0edf3ac544a5a14e377ed177d074ef
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-18 22:30:59 +0200

    Added AboutMe.vue and Contact.vue views

---
+++ src/views/AboutMe.vue
@@ -0,0 +1,20 @@
+<template>
+    <section id="AboutMe">
+        <h2 class="section--title">About Me</h2>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "AboutMe",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../global.scss";
+
+#AboutMe {
+    background-color: $background-secondary-color;
+    height: 100vh;
+}
+</style>
