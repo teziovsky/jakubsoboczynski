e945b38f231eaa70c4973a03e7b55cf4844f998d
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-18 22:31:26 +0200

    Added Technology.vue and Project.vue components contains each technology and project.

---
+++ src/components/Project.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="card">
+        <div class="card__info">
+            <h2 class="card__title">{{project.title}}</h2>
+            <span class="dot"></span>
+            <p class="card__description">{{project.description}}</p>
+        </div>
+        <div class="card__links">
+            <button class="card__links__item" @click.prevent="toggleScreen">SCREEN</button>
+            <a class="card__links__item" :href="project.github_link">SOURCE</a>
+            <a class="card__links__item" :href="project.demo_link">DEMO</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["project"],
+    name: "Project",
+    methods: {
+        toggleScreen() {
+            console.log("toggled");
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../global.scss";
+
+button {
+    color: $text-primary-color;
+    border: 0;
+    font: inherit;
+
+    &:focus {
+        outline: none;
+    }
+
+    &:hover {
+        cursor: pointer;
+    }
+}
+
+.card {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+    background-color: $background-third-color;
+    width: 350px;
+    margin: 25px;
+    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+
+    &__info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        padding: 30px 20px;
+    }
+
+    &__title {
+        font-size: 24px;
+        font-weight: 300;
+    }
+
+    &__description {
+        text-align: justify;
+    }
+
+    &__links {
+        display: flex;
+        justify-content: space-around;
+        border-top: 1px solid rgba($color: $text-primary-color, $alpha: 0.2);
+
+        &__item {
+            font-size: 14px;
+            font-weight: 300;
+            padding: 5px 13px;
+            margin: 20px 17px;
+            background: none;
+            transition: color 0.25s, border 0.25s ease;
+            position: relative;
+
+            &:hover {
+                color: $text-third-color;
+            }
+
+            &::before,
+            &::after {
+                display: block;
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 0;
+                height: 0;
+                border: 1px solid transparent;
+            }
+
+            &:hover::before,
+            &:hover::after {
+                width: 100%;
+                height: 100%;
+            }
+
+            &:hover::before {
+                border-top-color: $text-third-color;
+                border-right-color: $text-third-color;
+                transition: height 0.25s ease-out 0.25s, width 0.25s ease-out;
+            }
+
+            &:hover::after {
+                border-bottom-color: $text-third-color;
+                border-left-color: $text-third-color;
+                transition: height 0.25s ease-out, width 0.25s ease-out 0.25s;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/Technology.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="card">
+        <div class="card__image">
+            <img :src="require('../assets/technologies/' + technology.image + '.png')" alt="html" />
+        </div>
+        <div class="card__info">
+            <h2 class="card__title">{{technology.name}}</h2>
+            <span class="dot"></span>
+            <p class="card__description">{{technology.subtitle}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["technology"],
+    name: "Technology",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../global.scss";
+
+.card {
+    display: flex;
+    position: relative;
+    width: 550px;
+    margin: 20px 25px 20px 0;
+
+    &__image {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 130px;
+        height: 130px;
+        background-color: $background-secondary-color;
+        box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+        position: absolute;
+        top: 50%;
+        left: 25px;
+        transform: translateY(-50%);
+
+        & > img {
+            width: 110px;
+            height: 110px;
+        }
+    }
+
+    &__info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: $background-third-color;
+        min-height: 150px;
+        margin-left: 140px;
+        padding: 20px 20px 20px 25px;
+        box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+    }
+
+    &__title {
+        font-size: 24px;
+        font-weight: 300;
+    }
+
+    &__description {
+        text-align: justify;
+    }
+}
+</style>
