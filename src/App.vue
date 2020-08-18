476ed6d05bc0d41bad4f8c446f9b6ce2de4b126e
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-18 22:30:32 +0200

    Added main file App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Hero />
+        <AboutMe />
+        <Technologies />
+        <Projects />
+        <Contact />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Hero from "./views/Hero";
+import AboutMe from "./views/AboutMe";
+import Technologies from "./views/Technologies";
+import Projects from "./views/Projects";
+import Contact from "./views/Contact";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+    name: "App",
+    components: {
+        Hero,
+        AboutMe,
+        Technologies,
+        Projects,
+        Contact,
+    },
 };
 </script>
 
 <style lang="scss">
+@import "./global.scss";
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html {
+    scroll-behavior: smooth;
+}
+
+a {
+    text-decoration: none;
+    color: $text-primary-color;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background: $background-primary-color;
+    color: $text-primary-color;
+    font-family: "Raleway", sans-serif;
+    font-variant: small-caps;
+}
+
+.wrapper {
+    max-width: 1800px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+.section--title {
+    font-size: 64px;
+    font-weight: 100;
+    text-transform: uppercase;
+    overflow: hidden;
+    text-align: center;
+    padding: 50px 0 20px;
+
+    &::before,
+    &::after {
+        background-color: $text-primary-color;
+        content: "";
+        display: inline-block;
+        height: 1px;
+        position: relative;
+        vertical-align: middle;
+        width: 50%;
+    }
+
+    &:before {
+        right: 0.5em;
+        margin-left: -50%;
+    }
+
+    &:after {
+        left: 0.5em;
+        margin-right: -50%;
+    }
+}
+
+.dot {
+    height: 1px;
+    width: 30px;
+    background-color: rgba($color: $text-primary-color, $alpha: 0.4);
+    display: inline-block;
+    margin: 10px 0 15px;
 }
 </style>
