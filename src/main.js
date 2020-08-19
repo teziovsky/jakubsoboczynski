b4125afbfd8e95452ec12a1d35afdc8caa8cbfee
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-19 21:09:05 +0200

    Added animate on scroll plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+AOS.init();
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
