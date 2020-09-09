6f4dfb76754401790592683f6db36369690bb4b1
Jakub Soboczyński <teziovsky@gmail.com>
2020-09-09 22:03:46 +0200

    Added note component import and informations about me

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,12 +1,44 @@
 <template>
     <section id="AboutMe">
         <h1 class="section--title">About Me</h1>
+        <h3>
+            Hi! My name is Jakub Soboczyński.
+            <span>All informations about me in few notes below!</span>
+        </h3>
+        <div class="wrapper">
+            <Note v-for="note in notes" :key="note.title" :note="note" />
+        </div>
     </section>
 </template>
 
 <script>
+import Note from "../components/Note";
 export default {
     name: "AboutMe",
+    data() {
+        return {
+            notes: [
+                {
+                    title: "general",
+                    description:
+                        "I'm a final year student of WSB University in Poznan in the faculty of informatics. I currently live in Plewiska near Poznań and I focus all my attention on developing my skills and learning about new technologies that will make my projects even better.",
+                },
+                {
+                    title: "experience",
+                    description:
+                        "I have no commercial experience, but I still improve my programming skills by writing new web applications. My main goal is to find a job as a frontend developer that will help me gain experience needed in everyday work faster.",
+                },
+                {
+                    title: "interests",
+                    description:
+                        "I love electronic music festivals, especially techno, trance and house sub genres. I also like to read news about android-based mobile devices and all new IT articles collected in the news aggregator - Inoreader. Since I bought my second car, I'm also interested in cars.",
+                },
+            ],
+        };
+    },
+    components: {
+        Note,
+    },
 };
 </script>
 
@@ -15,9 +47,32 @@ export default {
 
 #AboutMe {
     background-color: $background-secondary-color;
-    height: 100vh;
 }
 
-@media screen and (max-width: 500px) {
+h3 {
+    text-align: center;
+    padding: 10px;
+    text-align: center;
+    font-weight: 300;
+    font-size: 30px;
+    margin-bottom: 20px;
+    font-variant: small-caps;
+
+    & span {
+        display: block;
+        font-size: 22px;
+    }
+}
+
+@media screen and (max-width: 768px) {
+}
+
+@media screen and (max-width: 420px) {
+}
+
+@media screen and (max-width: 375px) {
+}
+
+@media screen and (max-width: 320px) {
 }
 </style>
