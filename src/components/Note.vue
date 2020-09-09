2645b306a6adc388cb6d6eb86752c5a0e8824009
Jakub Soboczyński <teziovsky@gmail.com>
2020-09-09 21:59:27 +0200

    Added note component

---
+++ src/components/Note.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="note">
+        <p class="note__title">{{note.title}}</p>
+        <p class="note__description">{{note.description}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["note"],
+    name: "Note",
+};
+</script>
+
+<style lang="scss">
+@import "../global.scss";
+
+.note {
+    width: 350px;
+    min-height: 150px;
+    padding: 25px 20px 20px 20px;
+    margin: 20px 10px 44px 10px;
+    position: relative;
+    color: $text-primary-color;
+    background: $background-third-color;
+    line-height: 20px;
+    text-align: center;
+    box-shadow: 0 4px 5px rgba($background-primary-color, 0.4);
+
+    &:before {
+        display: block;
+        content: "";
+        line-height: 30px;
+        background: rgba($text-third-color, 0.5);
+        width: 130px;
+        height: 28px;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+        border-radius: 6px/18px 0;
+        position: absolute;
+        top: -13px;
+        left: 50%;
+        transform: rotate(-2deg);
+        transform: translateX(-50%);
+    }
+
+    &__title {
+        position: absolute;
+        top: -13px;
+        left: 50%;
+        transform: translateX(-50%);
+        color: rgba($color: $background-third-color, $alpha: 0.8);
+        text-transform: lowercase;
+        font-weight: 500;
+        line-height: 30px;
+    }
+
+    &__description {
+        text-align: justify;
+    }
+}
+</style>
