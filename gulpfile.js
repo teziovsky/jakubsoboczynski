274cdf5015b80f3856cd9a6fd184a0702d69d726
Jakub Soboczyński <teziovsky@gmail.com>
2020-10-10 10:03:23 +0200

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,6 @@ const autoprefixer = require("gulp-autoprefixer");
 
 exports.default = () =>
   gulp
-    .src("dist/css")
-    .pipe(
-      autoprefixer({
-        cascade: false
-      })
-    )
-    .pipe(gulp.dest("dist/css"));
+    .src("./dist/css/*.css")
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./dist/css/csss"));
