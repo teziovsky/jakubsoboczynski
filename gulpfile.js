af9a92e62b7b139dbd8370420988b8201b13151e
Kuba <kuba@MacBook-Pro-Kuba.local>
2020-12-01 18:39:05 +0100

    Changed gitignore file

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-
-exports.default = () =>
-  gulp
-    .src("./dist/css/*.css")
-    .pipe(autoprefixer())
-    .pipe(gulp.dest("./dist/css/csss"));
