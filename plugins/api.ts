6927af9681e40ceb4d0a274a4606745124a1628b
Jakub Soboczyński <teziovsky@gmail.com>
2022-10-16 22:29:57 +0200

    refactor: remove unnecessary files

--- plugins/api.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "contentful";
-
-const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
-
-export const api = createClient({
-  space: space || "",
-  accessToken: accessToken || "",
-});
