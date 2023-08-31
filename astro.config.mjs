f92adfba547160a7e765d7b73a10227b3ba0328d
Jakub Soboczyński <teziovsky@gmail.com>
2023-08-31 19:41:40 +0200

    feat: add last modified, external links and time read

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,19 @@ import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import { remarkModifiedTime } from "./src/lib/remark-modified-time.cjs";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.cjs";
+import rehypeExternalLinks from "rehype-external-links";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jakubsoboczynski.pl",
+  markdown: {
+    remarkPlugins: [remarkModifiedTime, remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }]],
+  },
   integrations: [
     compress(),
     tailwind(),
---
+++ src/lib/remark-modified-time.cjs
@@ -0,0 +1,9 @@
+import { statSync } from "fs";
+
+export function remarkModifiedTime() {
+  return function (tree, file) {
+    const filepath = file.history[0];
+    const result = statSync(filepath);
+    file.data.astro.frontmatter.lastModified = result.mtime.toISOString();
+  };
+}
